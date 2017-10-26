79949709c9c6e8adc27564c7c163127aa4b2ea13
shengun <1599775998@qq.com>
2017-10-26 16:57:40 +0800

    定时任务调度

--- src/main/java/com/job/MyJob.java
+++ src/main/java/com/job/MyJob.java
@@ -1,20 +1,30 @@
 package com.job;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 /**
  * quartz定时任务调度器
  * 
  * @author admin
  *
  */
+@Component
 public class MyJob  {
 	
 	/**
 	 * 可引入外部service
 	 */
+	@Scheduled(cron="0/5 * * * * ? ")
 	public void execute() {
 		// 要实现的具体的业务逻辑
 		System.out.println("haha此时此刻我们的任务终于要开始了。。。。。。。。。");
 
 	}
-    
+	@Scheduled(cron="0/6 * * * * ? ")
+	public void execute2() {
+		// 要实现的具体的业务逻辑
+		System.out.println("haha此时此刻我们的任务终于要开始了。。。。。。。。。");
+
+	}
 }
--- src/test/java/test/QuartzTest.java
+++ src/test/java/test/QuartzTest.java
@@ -6,24 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import com.baseutil.BaseJunitUtil;
+import com.job.MyJob;
+
 /**
  * 任务调度
+ * 
  * @author admin
  *
  */
 public class QuartzTest extends BaseJunitUtil {
-     
-	  @Autowired
-	  SchedulerFactoryBean schedulerFactory;
-	  
-	  @Test
-	  public void execute(){
-		   
-		   try {
+
+	@Autowired
+	MyJob job;
+
+	@Test
+	public void execute() {
+		//注解的方式
+		job.execute();
+		
+		
+		/**
+                     配置文件的方式
+		try {
 			schedulerFactory.getScheduler().start();
 		} catch (SchedulerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-	  }
+		}*/
+	}
 }
