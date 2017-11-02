046adf00e057ac1f91b566be3033fd1a6acf1d28
shengun <1599775998@qq.com>
2017-11-02 15:16:26 +0800

    略变动

--- src/test/java/test/Test2.java
+++ src/test/java/test/Test2.java
@@ -1,7 +1,6 @@
 package test;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
