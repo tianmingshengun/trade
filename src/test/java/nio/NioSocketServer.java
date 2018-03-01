5d41ada87cd7f357506638a86f16876a8a5e7a3a
shengun <1599775998@qq.com>
2018-03-01 18:05:29 +0800

    改动

--- src/test/java/nio/NioSocketServer.java
+++ src/test/java/nio/NioSocketServer.java
@@ -17,7 +17,7 @@ import java.util.Set;
 public class NioSocketServer {
 	public static void main(String[] args) throws Exception {  
         //定义缓存区，并指定容量大小
-		ByteBuffer buffer = ByteBuffer.allocate(200);  
+		ByteBuffer buffer = ByteBuffer.allocate(1024);  
         //创建服务器套接字通道，线程安全的
 		ServerSocketChannel ssc = ServerSocketChannel.open();           
         ssc.configureBlocking(false);//与选择器配合使用，通道需要设置为非阻塞模式  
@@ -25,7 +25,7 @@ public class NioSocketServer {
         //创建选择器，用于检测一到多个NIO通道，从而使单个线程可以管理多个通道，减少线程上下文切换带来的资源消耗
         Selector selector = Selector.open(); 
         //创建套接字地址
-        InetSocketAddress address = new InetSocketAddress(8080);  
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1",6668);  
         //为该套接字绑定地址
         ss.bind(address);  
         //注册套接字
@@ -33,22 +33,22 @@ public class NioSocketServer {
         System.out.println("开始监听……");  
         while (true) {  
             int num = selector.select();//返回已经准备就绪的通道
-            if(num==0){
-            	continue;
-            }
-           //获取有效键集
+            
+           //获取有效键集,检测通道所触发的事件
             Set selectedKeys = selector.selectedKeys();  
             Iterator it = selectedKeys.iterator();  
             //持续监控这个selectedKey的四种事件（接受，连接，读，写）是否就绪
             while (it.hasNext()) {  
                 SelectionKey sKey = (SelectionKey) it.next();  
+                it.remove();  
                 SocketChannel channel = null;  
                 if (sKey.isAcceptable()) {  
                     ServerSocketChannel sc = (ServerSocketChannel) key.channel();  
                     channel = sc.accept();// 接受连接请求  
+                    //channel.write(buffer.wrap(new String("sendddd").getBytes()));
                     channel.configureBlocking(false);  
                     channel.register(selector, SelectionKey.OP_READ);  
-                    it.remove();  
+                   
                 } else if (sKey.isReadable()) {  
                     channel = (SocketChannel) sKey.channel();  
                     while (true) {  
@@ -62,10 +62,8 @@ public class NioSocketServer {
                         System.out.println("##" + r + " " + new String(buffer.array(), 0, buffer.position())+"haha");  
                         buffer.flip();  
                     }  
-                    it.remove();  
-                } else {  
-                    channel.close();  
-                }  
+                      
+                } 
             }  
         }  
   
--- src/test/java/nio/NioTest.java
+++ src/test/java/nio/NioTest.java
@@ -1,49 +1,64 @@
 package nio;
-import java.net.InetSocketAddress;  
-import java.nio.ByteBuffer;  
-import java.nio.channels.SelectionKey;  
-import java.nio.channels.Selector;  
-import java.nio.channels.SocketChannel;  
-import java.util.Iterator;  
-import java.util.Set;  
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * nio客户端
+ * 
  * @author admin
  *
  */
 public class NioTest {
-	 public static void main(String[] args) throws Exception {  
-	        ByteBuffer echoBuffer = ByteBuffer.allocate(1024);  
-	        SocketChannel channel = null;  
-	        Selector selector = null;  
-	        channel = SocketChannel.open();  
-	        channel.configureBlocking(false);  
-	        // 请求连接  
-	        channel.connect(new InetSocketAddress("localhost", 8080));  
-	        selector = Selector.open();  
-	        channel.register(selector, SelectionKey.OP_CONNECT);  
-	        int num = selector.select();  
-	        Set selectedKeys = selector.selectedKeys();  
-	        Iterator it = selectedKeys.iterator();  
-	        while (it.hasNext()) {  
-	            SelectionKey key = (SelectionKey) it.next();  
-	            it.remove();  
-	            if (key.isConnectable()) {  
-	                if (channel.isConnectionPending()) {  
-	                    if (channel.finishConnect()) {  
-	                        // 只有当连接成功后才能注册OP_READ事件  
-	                        key.interestOps(SelectionKey.OP_READ);  
-	                        echoBuffer.put("123456789abcdefghijklmnopq".getBytes());  
-	                        echoBuffer.flip();  
-	                        System.out.println("##" + new String(echoBuffer.array()));  
-	                        channel.write(echoBuffer);  
-	                        System.out.println("写入完毕");  
-	                    } else {  
-	                        key.cancel();  
-	                    }  
-	                }  
-	            }  
-	        }  
-	  
-	    }  
+	public static void main(String[] args) throws Exception {
+		//创建一个客户端通道
+		SocketChannel socketChannel =  SocketChannel.open();
+		//连接远程主机
+		socketChannel.connect(new InetSocketAddress("127.0.0.1",6668));
+		socketChannel.configureBlocking(false);
+		//声明选择器
+		Selector selector=Selector.open();
+		//将通道注册到选择器上，并指定检测的事件
+		socketChannel.register(selector, SelectionKey.OP_CONNECT);
+		//轮询监测发生的事件
+		 System.out.println("连接成功");
+		while(true){
+			//查看是否有已经准备就绪的通道，该方法会产生阻塞
+		    int num= selector.select();
+		  //  if(num==0) continue;
+		    //获取和通道关联的事件
+		    Set<SelectionKey> set=selector.selectedKeys();
+		    Iterator<SelectionKey> iterator=set.iterator();
+		    //检测相关事件并进行处理
+		   
+		    while(iterator.hasNext()){
+		    	SelectionKey sk= iterator.next();
+		    	//删除已选择的键，set集合不会自动处理处理完的键
+		    	iterator.remove();
+		    	//判读该事件是否被触发
+		    	if(sk.isConnectable()){
+		    		
+		    		SocketChannel channel=(SocketChannel)sk.channel();
+		    		//判断该通道是否正在连接
+		    		if(channel.isConnectionPending()){
+		    			if(channel.finishConnect()){
+		    				System.out.println("连接成功");
+		    			}
+		    		}
+		    		channel.configureBlocking(false);
+		    		//发送信息
+		    		channel.write(ByteBuffer.wrap("请多关照。。".getBytes()));
+		    		//注册读取事件，用于获取服务端的信息
+		    		channel.register(selector,  SelectionKey.OP_READ);
+		    		
+		    	}
+		    }
+		}
+	}
 }
